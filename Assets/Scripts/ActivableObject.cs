dde45c6e42f54b13e5ab114c592829a776e7e074
ETU\f.birette <f.birette@rubika-edu.com>
2017-05-18 19:40:07 +0200

    Jolies portes

--- Assets/Scripts/ActivableObject.cs
+++ Assets/Scripts/ActivableObject.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class ActivableObject : MonoBehaviour {
 
-	public Vector3 destination;
-	Vector3 origin;
+
+	public DoorColor doorColor;
+	public bool state;
+
+	public GameObject blueDoorPrefab;
+	public GameObject redDoorPrefab;
+	public GameObject yellowDoorPrefab;
+
+	GameObject tmpObj;
+	DoorActivation doorActiv;
 
 	void Start(){
-		origin = transform.position;
+		switch (doorColor) {
+		case DoorColor.Blue:
+			tmpObj = Instantiate (blueDoorPrefab, transform.position, transform.rotation, transform);
+			break;
+		case DoorColor.Red:
+			tmpObj = Instantiate (redDoorPrefab, transform.position, transform.rotation, transform);
+			break;
+		case DoorColor.Yellow:
+			tmpObj = Instantiate (yellowDoorPrefab, transform.position, transform.rotation, transform);
+			break;
+		default:
+			tmpObj = Instantiate (blueDoorPrefab, transform.position, transform.rotation, transform);
+			break;
+		}
+		doorActiv = tmpObj.GetComponent<DoorActivation> ();
+		doorActiv.SetState (state);
 	}
 
 	public void Trigger(){
-		if (transform.position == origin){
-			transform.position = destination;
-		} else {
-			transform.position = origin;
-		}
+		state = !state;
+		doorActiv.SetState (state);
+	}
+
+	public enum DoorColor
+	{
+		Blue,
+		Red,
+		Yellow
 	}
 }
---
+++ Assets/Scripts/DoorActivation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorActivation : MonoBehaviour {
+
+
+	public List<GameObject> Lasers = new List<GameObject> ();
+
+	public void SetState(bool state){
+		foreach (GameObject item in Lasers) {
+			item.SetActive (state);
+		}
+	}
+
+}
