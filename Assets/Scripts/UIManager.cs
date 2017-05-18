e5a7afa46c31d91405a6154af269404c6ef572ae
grreuze <grreuze@gmail.com>
2017-05-18 11:11:11 +0200

    level selection wip

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
     SceneField levelToStart;
+    int currentIndex = 0;
+    public Level[] levels;
+
+    [SerializeField]
+    Image lvlImg;
+    [SerializeField]
+    TextMeshProUGUI lvlName;
+
+    public void Start() {
+
+    }
 
     public void StartLevel() {
         SceneManager.LoadScene(levelToStart);
@@ -14,4 +26,11 @@ public class UIManager : MonoBehaviour {
         Application.Quit();
     }
 
+}
+
+[System.Serializable]
+public struct Level {
+    public string name;
+    public Sprite screen;
+    public SceneField scene;
 }
