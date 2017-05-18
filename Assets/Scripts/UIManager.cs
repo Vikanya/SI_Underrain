8a64a4dcf58e5184756ed8054278f60e51f50588
grreuze <grreuze@gmail.com>
2017-05-18 10:30:55 +0200

    TextMeshPro

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    SceneField levelToStart;
+
+    public void StartLevel() {
+        SceneManager.LoadScene(levelToStart);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
