52b140e367123ab131650e02a6e90b1b5e9b07b1
Unknown <antoine.villecroze@gmail.com>
2017-05-19 12:07:24 +0200

    Title Screen Update (click toto) + pause menu graph fix

---
+++ Assets/Scripts/TotoButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotoButton : MonoBehaviour {
+
+	public Animator anim;
+	public List<GameObject> accessories = new List<GameObject>();
+	private int index;
+
+
+
+	public void ChangeAccessory()
+	{
+		Debug.Log ("coolbutton");
+		anim.SetTrigger ("Interaction");
+
+		accessories [index].SetActive (false);
+		index++;
+		if (index > accessories.Count-1) {
+			index = 0;
+		}
+		accessories [index].SetActive (true);
+	}
+}
